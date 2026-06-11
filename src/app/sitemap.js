eab1dc47b1417bde9d7ca435364cd23e6044b719
salmansahed <salmansahed10@gmail.com>
2026-06-11 13:56:53 +0600

    add sitemap.js for google indexing

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://salman-sahed.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+  ];
+}
